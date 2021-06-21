d519a77980eee99803b9c2c2d161273c3dd55b6a
Gabriel Viana <g.viana87@hotmail.com>
2021-06-21 13:45:26 -0300

    item 3 Na pasta src , crie um arquivo chamado Component.js crie um componente que retorne um <h1> com o seu nome um paragráfo, <p> , com uma breve descrição sobre você.

---
+++ front_end/boco_11/dia_1/classes_e_react/src/Component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class SobreMim extends Component {
+  reader() {
+    const header= (
+      <header className='flex-container'>
+
+      </header>
+    )
+  }
+}
