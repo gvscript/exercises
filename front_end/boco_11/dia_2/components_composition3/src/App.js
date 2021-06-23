08df61fade3a560b152bab7dfbf6599efac02758
Gabriel Viana <g.viana87@hotmail.com>
2021-06-23 12:45:17 -0300

    troca avatar

--- front_end/boco_11/dia_2/components_composition3/src/App.js
+++ front_end/boco_11/dia_2/components_composition3/src/App.js
@@ -9,13 +9,13 @@ class App extends React.Component {
         id: 102,
         name: "João",
         email: "joao@gmail.com",
-        avatar: "https:cdn.pixabay.com/photo/2016/08/20/05/38/avatar-1606916_640.png"
+        avatar: "https://i.pinimg.com/originals/74/69/85/746985ab750d62c9605322b2fef77184.jpg"
       },
       {
         id: 77,
-        name: "Amélia",
-        email: "amelia@gmail.com",
-        avatar: "https:cdn.pixabay.com/photo/2017/01/31/13/05/cameo-2023867_640.png"
+        name:"Dandara",
+        email:"dandara@gmail.com",
+        avatar:"https://static.vecteezy.com/ti/vetor-gratis/p1/1834070-personagem-jovem-forte-africana-mulher-feminina-avatar-gr%C3%A1tis-vetor.jpg"
       }
     ];
 
