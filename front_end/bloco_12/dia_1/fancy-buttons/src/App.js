489fe39cda258849a954f879cf482dade0b40d11
Gabriel Viana <g.viana87@hotmail.com>
2021-06-26 11:35:10 -0300

    criada function de açao para cada button

--- front_end/bloco_12/dia_1/fancy-buttons/src/App.js
+++ front_end/bloco_12/dia_1/fancy-buttons/src/App.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 
+function handleClick1() {
+  console.log('Botão 1 ok');
+};
+function handleClick2() {
+  console.log('Botão 2 ok');
+};
+function handleClick3() {
+  console.log('Botão 3 ok');
+};
+
 class App extends Component {
   render() {
     return (
