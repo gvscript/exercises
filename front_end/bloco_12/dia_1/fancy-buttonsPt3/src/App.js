efcf7f9f47e695e6bc075af9fee32b7ec2b7c102
Gabriel Viana <g.viana87@hotmail.com>
2021-06-26 15:41:19 -0300

    exercicio feito novamente desde o inicio - fixaçao ok

--- front_end/bloco_12/dia_1/fancy-buttonsPt3/src/App.js
+++ front_end/bloco_12/dia_1/fancy-buttonsPt3/src/App.js
@@ -6,42 +6,41 @@ class App extends Component {
     super()
 
     this.state = {
-      numeroCliques1: 0,
-      numeroCliques2: 0,
-      numeroCliques3: 0,
-    }
+      numClick1: 0,
+      numClick2: 0,
+      numClick3: 0,
+    };
 
     this.hendleClick1 = this.hendleClick1.bind(this);
     this.hendleClick2 = this.hendleClick2.bind(this);
     this.hendleClick3 = this.hendleClick3.bind(this);
-  }
+  };
 
   hendleClick1() {
     this.setState((numAnterior, _props) => ({
-      numeroCliques1: numAnterior.numeroCliques1 + 1
+      numClick1: numAnterior.numClick1 +1
     }));
-  
-  }
+  };
 
   hendleClick2() {
     this.setState((numAnterior, _props) => ({
-      numeroCliques2: numAnterior.numeroCliques2 + 1
-    }))
-  }
+      numClick2: numAnterior.numClick2 +1
+    }));
+  };
 
   hendleClick3() {
     this.setState((numAnterior, _props) => ({
-      numeroCliques3: numAnterior.numeroCliques3 + 1
-    }))
-  }
+      numClick3: numAnterior.numClick3 +1
+    }));
+  };
 
   render() {
     console.log(this);
     return(
       <section>
-        <button onClick={ this.hendleClick1 }>B1 - clicou { this.state.numeroCliques1 }</button>
-        <button onClick={ this.hendleClick2 }>B2 - clicou { this.state.numeroCliques2 }</button>
-        <button onClick={ this.hendleClick3 }>B3 - clicou { this.state.numeroCliques3 }</button>
+        <button onClick={ this.hendleClick1 }>B1 - clicou { this.state.numClick1 }</button>
+        <button onClick={ this.hendleClick2 }>B2 - clicou { this.state.numClick2 }</button>
+        <button onClick={ this.hendleClick3 }>B3 - clicou { this.state.numClick3 }</button>
       </section>
     )
   }
