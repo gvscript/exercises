6064f71ec1abd61f91d57759b0f56d50dcd9e6e4
Gabriel Viana <g.viana87@hotmail.com>
2021-06-28 12:56:34 -0300

    implementado fixaçao conteudo event hendle genericos - transformando todos os elementos do formulario em componentes controlados com apenas uma funçao hendle

--- front_end/bloco_12/dia_2/componentes_controlados/src/Components/Form.jsx
+++ front_end/bloco_12/dia_2/componentes_controlados/src/Components/Form.jsx
@@ -1,55 +1,81 @@
 import React, { Component } from 'react';
-import './Form.css';
+import './Form.css'
 
 class Form extends Component {
   constructor() {
     super()
 
-    this.hendleChangeTextArea = this.hendleChangeTextArea.bind(this);
+    this.hendleChenge = this.hendleChenge.bind(this);
 
     this.state = {
-      estado: '',
-      sobreVoce: '',
+      nome: '',
+      email: '',
       idade: 0,
-      aceite: false,
-    }
-    console.log();
-  };
-
-  hendleChangeTextArea(e) {
-    this.setState({ sobreVoce: e.target.value });
+      hub: '',
+      sobreVoce: '',
+      confirma: false,
+    };
   }
 
+  hendleChenge(e) {
+    const { name } = e.target
+    const value = e.target.type === 'checkbox' ? e.target.checked : e.target.value;
+    this.setState({ [name]: value })
+  }
 
   render() {
     return(
       <section>
         <h1>Formulário em React</h1>
         <form className='form'>
-          <select name="estado">
+          <input 
+            placeholder='Informe seu nome'
+            type="text" 
+            name="nome"
+            value={ this.state.nome }
+            onChange={ this.hendleChenge }
+          />
+          <input
+            placeholder='Informe seu email'
+            type="email" 
+            name="email"
+            value={ this.state.email }
+            onChange={ this.hendleChenge }
+          />
+          <input 
+            placeholder='Informe sua idade'
+            type="number" 
+            name="idade"
+            value={ this.state.idade }
+            onChange={ this.hendleChenge }
+          />
+          <p>Qual seu Hub?</p>
+          <select name="hub" value={ this.state.hub } onChange={ this.hendleChenge }>
             <option value="sp">São Paulo</option>
             <option value="bh">Belo Horizonte</option>
           </select>
-          <input 
-            type='number'
-            name="idade" 
-          />
-          <input 
-            type="checkbox" 
-            name="aceite"
-          />
           <label htmlFor="">
-            Sobre você:
+            <p>Sobre você:</p>
             <textarea 
-              name="sobreVoce" 
-              value={ this.state.sobreVoce } 
-              onChange={ this.hendleChangeTextArea }
+              name="sobreVoce"
+              value={ this.state.sobreVoce }
+              onChange={ this.hendleChenge }
+            />
+          </label>
+          <label htmlFor="">
+            Confirma todos os dados?
+            <input 
+              type="checkbox" 
+              name="confirma"
+              value={ this.state.confirma }
+              onChange={ this.hendleChenge }
             />
           </label>
         </form>
       </section>
     )
   }
+
 }
 
 export default Form;
